737bb357c4be2c86f111da6191df795e3aa7750b
hamid-ali <hamidali4218867@gmail.com>
2025-10-29 21:39:52 +0500

    chang 2.2

--- src/app/api/getipaddress/route.ts
+++ src/app/api/getipaddress/route.ts
@@ -1,25 +1,25 @@
-// pages/api/getipaddress.ts
-import type { NextApiRequest, NextApiResponse } from "next";
+// // pages/api/getipaddress.ts
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+// export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
-  try {
-    const xff = req.headers["x-forwarded-for"] as string | undefined;
-    let ip = "";
+//   try {
+//     const xff = req.headers["x-forwarded-for"] as string | undefined;
+//     let ip = "";
 
-    if (xff) {
-      ip = xff.split(",")[0].trim();
-    } else if (req.socket?.remoteAddress) {
-      ip = req.socket.remoteAddress || "";
-    }
+//     if (xff) {
+//       ip = xff.split(",")[0].trim();
+//     } else if (req.socket?.remoteAddress) {
+//       ip = req.socket.remoteAddress || "";
+//     }
 
-    if (ip?.startsWith("::ffff:")) {
-      ip = ip.replace("::ffff:", "");
-    }
+//     if (ip?.startsWith("::ffff:")) {
+//       ip = ip.replace("::ffff:", "");
+//     }
 
-    res.status(200).json({ success: true, ip });
-  } catch (error: any) {
-    console.error("IP fetch error:", error);
-    res.status(500).json({ success: false, message: error.message });
-  }
-}
+//     res.status(200).json({ success: true, ip });
+//   } catch (error: any) {
+//     console.error("IP fetch error:", error);
+//     res.status(500).json({ success: false, message: error.message });
+//   }
+// }
